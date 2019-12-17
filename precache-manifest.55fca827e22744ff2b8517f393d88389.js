8e269e6cc94af2ba0e4fe4908338924a5368633e
Adrian Express <contact@adrian.express>
2019-12-17 15:37:04 +0000

    Updates

---
+++ precache-manifest.55fca827e22744ff2b8517f393d88389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4fcc18713436f40de0b36b5aefe956",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "09ae3ffca9705b8d9970",
+    "url": "/monsters-rolodex/static/css/main.df61f4a1.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09ae3ffca9705b8d9970",
+    "url": "/monsters-rolodex/static/js/main.310ae7b2.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
